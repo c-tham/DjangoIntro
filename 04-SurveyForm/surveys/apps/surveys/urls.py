002b577b3893b420cb8d8c28102597beac45bee5
Chia T <tham_oh@hotmail.com>
2018-02-10 03:22:45 -0600

    add logic

--- 04-SurveyForm/surveys/apps/surveys/urls.py
+++ 04-SurveyForm/surveys/apps/surveys/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
  
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^process$', views.process), ### add this line
+    url(r'^result$', views.result), ### add this line
+    url(r'^reset$', views.reset), ### add this line
 ]
--- 04-SurveyForm/surveys/apps/surveys/views.py
+++ 04-SurveyForm/surveys/apps/surveys/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render, HttpResponse, redirect
+
 def index(request):
-    context = {
-        "name" : "surveys"
-    }
-    return render(request, "surveys/index.html", context)
+    if 'idx' not in request.session:
+        request.session['idx'] = 0
+    return render(request, "surveys/index.html")
+
+def process(request):
+    if request.method == "POST":
+        request.session['idx'] += 1 
+        request.session['name'] = request.POST['name']
+        request.session['location'] = request.POST['location']
+        request.session['language'] = request.POST['language']
+        request.session['comment'] = request.POST['comment']
+        return render(request,'surveys/result.html')
+    else:
+        return redirect("/")
+
+def result(request):
+    return redirect('/')
+
+def reset(request):
+    request.session['idx'] = 0
+    request.session['name'] = ''
+    request.session['location'] = ''
+    request.session['language'] = ''
+    request.session['comment'] = ''
+    return redirect("/")
